a9f39e0cbc2647d1e6ac2dbf37892dfaceffaeed
neelbavarva <neelbavarva5@gmail.com>
2021-10-13 01:58:37 +0530

    blog frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home/Home'
+
+export default function App() {
+    return (
+        <div>
+            <Home />
+        </div>
+    );
+}
